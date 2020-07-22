456369b922678c9593db28d3a9a857fb39f6b343
Carlos <filoxo@users.noreply.github.com>
2020-07-22 13:43:50 -0600

    Feat/implement (#1)
    * Add emotion dependency
    
    * Replace name throughout
    
    * Install dependencies
    
    * Update version, description
    
    * Bundle and build
    
    * Add tests
    
    * Validate that the API actually functions
    
    * Move publishConfig into package.json per documentation here https://github.com/release-it/release-it/blob/master/docs/npm.md\#public-scoped-packages
    
    * Update links

--- browser-test/test.js
+++ browser-test/test.js
@@ -1,9 +1,41 @@
-describe("@esm-bundle/autopublish-template", () => {
-  it("can load the ESM bundle", () => {
-    return import("/base/esm/index.js");
-  });
+describe("@esm-bundle/emotion", () => {
+  [
+    ["development ESM", import("/base/esm/emotion.js")],
+    ["production ESM ", import("/base/esm/emotion.min.js")],
+    ["development System.register", System.import("/base/system/emotion.js")],
+    [
+      "production System.register",
+      System.import("/base/system/emotion.min.js"),
+    ],
+  ].forEach(([moduleType, importFn]) => {
+    it(`can load the ${moduleType} bundle`, () => {
+      return importFn.then((module) => {
+        expect(module).toBeDefined();
+        const { css, cx } = module;
+        expect(css).toBeDefined();
+        expect(cx).toBeDefined();
+      });
+    });
 
-  it("can load the System.register bundle", () => {
-    return System.import("/base/system/index.js");
+    it("the API generates classNames", () => {
+      return System.import("/base/system/emotion.js").then(({ css, cx }) => {
+        // Basic test that matches simple example https://emotion.sh/docs/introduction#framework-agnostic
+        const color = "white",
+          base = css`
+            border-radius: 4px;
+            padding: 32px;
+            &:hover {
+              color: ${color};
+            }
+          `,
+          extra = css`
+            background-color: hotpink;
+            font-size: 24px;
+          `;
+
+        expect(base).toContain("css-");
+        expect(cx(base, extra)).toContain("css-");
+      });
+    });
   });
 });
