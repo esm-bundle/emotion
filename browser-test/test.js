d978ea9f882599ceac4552aea4b94b5d2b100498
Carlos Filoteo <c.filoteo12@gmail.com>
2020-07-22 12:35:42 -0600

    Validate that the API actually functions

--- browser-test/test.js
+++ browser-test/test.js
@@ -16,5 +16,26 @@ describe("@esm-bundle/emotion", () => {
         expect(cx).toBeDefined();
       });
     });
+
+    it("the API generates classNames", () => {
+      return System.import("/base/system/emotion.js").then(({ css, cx }) => {
+        // Basic test that matches simple example https://emotion.sh/docs/introduction#framework-agnostic
+        const color = "white",
+          base = css`
+            border-radius: 4px;
+            padding: 32px;
+            &:hover {
+              color: ${color};
+            }
+          `,
+          extra = css`
+            background-color: hotpink;
+            font-size: 24px;
+          `;
+
+        expect(base).toContain("css-");
+        expect(cx(base, extra)).toContain("css-");
+      });
+    });
   });
 });
