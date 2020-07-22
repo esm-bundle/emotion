ccfa3054f7f8124ffdf94412cde131154fa1feba
Carlos Filoteo <c.filoteo12@gmail.com>
2020-07-22 12:08:30 -0600

    Bundle and build

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,35 @@
-import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
 import { terser } from "rollup-plugin-terser";
 
-function createConfig(format) {
+function createConfig(format, options = {}) {
   const dir = format === "module" ? "esm" : format;
+  const { min = false } = options;
+
   return {
-    input: require.resolve("emotion"),
+    input: `src/index.js`,
     output: {
-      file: `${dir}/index.js`,
+      file: `${dir}/emotion${min ? ".min" : ""}.js`,
       sourcemap: true,
       format,
     },
-    plugins: [commonjs(), terser()],
+    plugins: [
+      nodeResolve(),
+      replace({
+        values: {
+          "process.env.NODE_ENV": JSON.stringify(
+            min ? "production" : "development"
+          ),
+        },
+      }),
+      min && terser(),
+    ],
   };
 }
 
-export default [createConfig("module"), createConfig("system")];
+export default [
+  createConfig("module"),
+  createConfig("module", { min: true }),
+  createConfig("system"),
+  createConfig("system", { min: true }),
+];
